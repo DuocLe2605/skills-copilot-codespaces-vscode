c2ad81fb67626fd9eaf59d084310f84ed559a96b
DuocLe2605 <167075326+DuocLe2605@users.noreply.github.com>
2024-06-11 08:06:57 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var skills = {
+        name: "John",
+        age: 25,
+        skills: ["C++", "Java", "Python"]
+    }
+    console.log(skills.name);
+    console.log(skills.age);
+    console.log(skills.skills);
+}
